7a7dda2c88b158476d8fc61cca8b8d01428be7cc
Daniel Toro Soto <daniel.toro1@utp.edu.co>
2025-12-22 10:22:34 -0500

    feat: Generate Android release build artifacts and add new project configuration and notification icon.

--- proxy.ts
+++ proxy.ts
@@ -13,9 +13,11 @@ export const config = {
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      * - manifest.json (PWA manifest)
+     * - icons/* (PWA icons)
+     * - .well-known/* (for assetlinks.json)
      * - images - .svg, .png, .jpg, .jpeg, .gif, .webp
      * Feel free to modify this pattern to include more paths.
      */
-    "/((?!_next/static|_next/image|favicon.ico|manifest\\.json|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    "/((?!_next/static|_next/image|favicon.ico|manifest\\.json|icons/|\\.well-known/|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 };
